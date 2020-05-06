c1ce452436921c18f70fd03b11ec6350baf18f6d
z3nth10n <z3nth10n@gmail.com>
2020-05-06 17:04:48 +0200

    Removed code

--- index.php
+++ index.php
@@ -6,16 +6,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Bike</title>
-    <!---
-    <link rel="stylesheet" href="https://sc.wklcdn.com/wikiloc/assets/styles/css/trailpage.min.24a89f08.css">
-    -->
     <link rel="stylesheet" href="https://sc.wklcdn.com/wikiloc/assets/styles/fonts/css/wikiloc-font.min.7a3ba9d0.css">
     <link rel="stylesheet" href="https://sc.wklcdn.com/wikiloc/assets/styles/css/elevation.min.a59f5e17.css">
-
-<!---
-    <link rel="stylesheet" href="css/fontawesome.min.css">
-    <link rel="stylesheet" href="css/all.min.css">
--->
 
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
